30c9d31d6e35f2a1986eb47af098a70121f9e469
DTHeaven <liu.hanhong@zuimeia.com>
2016-01-20 16:58:27 +0800

    Update sample.

--- AutoLayout/autolayout-sample/src/main/java/im/quar/autolayout/sample/AutoGridView.java
+++ AutoLayout/autolayout-sample/src/main/java/im/quar/autolayout/sample/AutoGridView.java
@@ -2,7 +2,7 @@ package im.quar.autolayout.sample;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.LinearLayout;
+import android.widget.GridView;
 
 import im.quar.autolayout.AutoLayoutSimple;
 
@@ -10,9 +10,10 @@ import im.quar.autolayout.AutoLayoutSimple;
  * Created by DTHeaven on 15/12/14.
  */
 @AutoLayoutSimple
-public class AutoLinearLayout extends LinearLayout {
+public class AutoGridView extends GridView {
 
-    public AutoLinearLayout(Context context, AttributeSet attrs) {
+    public AutoGridView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
+
 }
